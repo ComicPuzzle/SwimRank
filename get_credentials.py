a17d6acb7251772e6bb279c558683cb4e2ff5bdf
ComicPuzzle <alphadjw@gmail.com>
2025-12-22 13:20:04 -0500

    extra files

---
+++ get_credentials.py
@@ -0,0 +1,6 @@
+def get_credentials():
+    with open('credentials.txt', 'r') as file:
+        arr = []
+        for line in file:
+            arr.append(line.strip())
+    return (arr[0], arr[1], arr[2], arr[3])
